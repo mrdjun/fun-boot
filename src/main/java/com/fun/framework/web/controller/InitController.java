4099fa514dd6e64721a8a4015344867bcab4d067
mrdjun <mr.djun@qq.com>
2019-09-13 16:57:58 +0800

    🍊

--- src/main/java/com/fun/framework/web/controller/InitController.java
+++ src/main/java/com/fun/framework/web/controller/InitController.java
@@ -1,17 +1,17 @@
 package com.fun.framework.web.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * created by DJun on 2019/9/9 17:23
  * desc:
  */
-@RestController
+@Controller
 public class InitController {
     @GetMapping("")
     public String login(){
-        return "登录页面正在熬夜开发中...";
+        return "index";
     }
 
     @GetMapping("index")
