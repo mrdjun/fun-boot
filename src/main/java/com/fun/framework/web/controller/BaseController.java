1d974955cd4807df840880103a03b64eac95c801
mrdjun <mr.djun@qq.com>
2019-10-07 15:08:48 +0800

    🍊 完善BaseController的注释

--- src/main/java/com/fun/framework/web/controller/BaseController.java
+++ src/main/java/com/fun/framework/web/controller/BaseController.java
@@ -7,6 +7,7 @@ import com.fun.project.admin.system.entity.user.AdminUser;
 
 
 /**
+ * 此 Controller 可被直接继承并使用
  * @author DJun
  */
 public class BaseController {
@@ -18,18 +19,30 @@ public class BaseController {
         return StringUtils.format("redirect:{}", url);
     }
 
+    /**
+     * 获取当前用户全部信息
+     */
     public AdminUser getSysUser() {
         return ShiroUtils.getSysUser();
     }
 
+    /**
+     * 设置当前用户
+     */
     public void setSysUser(AdminUser user) {
         ShiroUtils.setSysUser(user);
     }
 
+    /**
+     * 获取当前用户的id
+     */
     public Long getUserId() {
         return getSysUser().getUserId();
     }
 
+    /**
+     * 获取当前用户登录的账号
+     */
     public String getLoginName() {
         return getSysUser().getLoginName();
     }
