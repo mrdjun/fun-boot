4237322ea3c1ef9410abc7e6969dfb1e508f0d72
mrdjun <mr.djun@qq.com>
2019-10-30 16:00:32 +0800

    🎨新增状态控制

--- src/main/java/com/fun/common/constant/Constants.java
+++ src/main/java/com/fun/common/constant/Constants.java
@@ -7,42 +7,29 @@ package com.fun.common.constant;
  * @author DJun
  */
 public class Constants {
-    /**
-     * 通用成功标识
-     */
-    public static final String SUCCESS = "0";
 
-    /**
-     * 通用失败标识
-     */
+    /**  通用成功、失败标识 */
+    public static final String SUCCESS = "0";
     public static final String FAIL = "1";
 
-    /**
-     * 登录成功
-     */
+    /** 登录状态 */
     public static final String LOGIN_SUCCESS = "登录成功";
-
-    /**
-     * 登录失败
-     */
     public static final String LOGIN_FAIL = "登录失败";
-
-    /**
-     * 注销
-     */
     public static final String LOGOUT = "注销成功";
 
-    /**
-     * 前端页面路径前缀
-     */
+    /** 前端页面路径前缀 */
     public static final String VIEW_PREFIX = "fun/views/";
+    public static String view(String view) {return VIEW_PREFIX + view;}
 
-    public static String view(String view) {
-        return VIEW_PREFIX + view;
-    }
-
-    /**
-     * 获取ip地区地址开关
-     */
+    /** 获取ip地区地址开关 */
     public static final boolean ADDRESS_ENABLED = true;
+
+    /** 字典类型是否唯一的返回结果码 */
+    public final static String UNIQUE = "0";
+    public final static String NOT_UNIQUE = "1";
+
+    /** 字典正常状态 */
+    public static final String DICT_NORMAL = "0";
+
+
 }
