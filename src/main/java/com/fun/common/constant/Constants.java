2b6f5ded24d465173696a77f52c7a9b6034de8b5
mrdjun <mr.djun@qq.com>
2019-10-31 20:44:08 +0800

    🎨优化分页处理

--- src/main/java/com/fun/common/constant/Constants.java
+++ src/main/java/com/fun/common/constant/Constants.java
@@ -19,7 +19,6 @@ public class Constants {
 
     /** 前端页面路径前缀 */
     public static final String VIEW_PREFIX = "fun/views/";
-    public static String view(String view) {return VIEW_PREFIX + view;}
 
     /** 获取ip地区地址开关 */
     public static final boolean ADDRESS_ENABLED = true;
@@ -33,5 +32,17 @@ public class Constants {
 
     /** 是否为系统默认（是） */
     public static final String YES = "Y";
+
+    /** 排序的方向 "desc" 或者 "asc" */
+    public static final String IS_ASC = "isAsc";
+
+    /** 排序列 */
+    public static final String ORDER_BY_COLUMN = "orderByColumn";
+
+    /** 当前记录起始索引 */
+    public static final String PAGE_NUM = "pageNum";
+
+    /** 每页显示记录数 */
+    public static final String PAGE_SIZE = "pageSize";
 
 }
--- src/main/java/com/fun/common/pagehelper/CommonPage.java
+++ src/main/java/com/fun/common/pagehelper/CommonPage.java
@@ -1,5 +1,8 @@
 package com.fun.common.pagehelper;
 
+import com.fun.common.constant.Constants;
+import com.fun.common.utils.ServletUtils;
+import com.fun.common.utils.StringUtils;
 import com.github.pagehelper.PageInfo;
 import lombok.Data;
 import org.springframework.data.domain.Page;
@@ -8,18 +11,22 @@ import java.util.List;
 
 /**
  * 分页数据封装类
+ *
  * @author DJun
  */
 @Data
 public class CommonPage<T> {
+
     /**
      * 页数
      */
     private Integer pageNum;
+
     /**
      * 每一页显示数据数
      */
     private Integer pageSize;
+
     /**
      * 总页数
      */
@@ -27,13 +34,25 @@ public class CommonPage<T> {
     private Long total;
     private List<T> list;
 
+    /**  排序列 */
+    private String orderByColumn;
+
+    /** 排序的方向 "desc" 或者 "asc". */
+    private String isAsc;
+
+    public String getOrderBy() {
+        if (StringUtils.isEmpty(orderByColumn)) {
+            return "";
+        }
+        return StringUtils.toUnderScoreCase(orderByColumn) + " " + isAsc;
+    }
+
     /**
      * 将PageHelper分页后的list转为分页信息
      */
     public static <T> CommonPage<T> restPage(List<T> list) {
         CommonPage<T> result = new CommonPage<T>();
         PageInfo<T> pageInfo = new PageInfo<T>(list);
-
         result.setTotalPage(pageInfo.getPages());
         result.setPageNum(pageInfo.getPageNum());
         result.setPageSize(pageInfo.getPageSize());
@@ -53,6 +72,22 @@ public class CommonPage<T> {
         result.setTotal(pageInfo.getTotalElements());
         result.setList(pageInfo.getContent());
         return result;
+    }
+
+    /**
+     * 封装分页对象
+     */
+    public static CommonPage getCommonPage() {
+        CommonPage commonPage = new CommonPage();
+        commonPage.setPageNum(ServletUtils.getParameterToInt(Constants.PAGE_NUM));
+        commonPage.setPageSize(ServletUtils.getParameterToInt(Constants.PAGE_SIZE));
+        commonPage.setOrderByColumn(ServletUtils.getParameter(Constants.ORDER_BY_COLUMN));
+        commonPage.setIsAsc(ServletUtils.getParameter(Constants.IS_ASC));
+        return commonPage;
+    }
+
+    public static CommonPage buildPageRequest() {
+        return getCommonPage();
     }
 
 }
