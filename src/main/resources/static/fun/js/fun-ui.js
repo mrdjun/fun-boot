eac13bbde765264733d0f6894a8d7754baa76407
mrdjun <mr.djun@qq.com>
2019-12-03 20:46:10 +0800

    🧀功能优化及完善app端管理

--- src/main/resources/static/fun/js/fun-ui.js
+++ src/main/resources/static/fun/js/fun-ui.js
@@ -480,7 +480,7 @@
             reset: function (formId) {
                 var currentId = $.common.isEmpty(formId) ? $('form').attr('id') : formId;
                 $("#" + currentId)[0].reset();
-                $.bttTable.bootstrapTable('refresh');
+                $.btTable.bootstrapTable('refresh');
             },
             // 获取选中复选框项
             selectCheckeds: function (name) {
