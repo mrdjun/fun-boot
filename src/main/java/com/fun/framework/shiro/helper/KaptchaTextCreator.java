b066485c46f5fa5edb5aa99b6a915ad841b29001
mrdjun <mr.djun@qq.com>
2019-12-08 12:45:54 +0800

    FUN-BOOT-v1.0.0正式发布

--- src/main/java/com/fun/framework/shiro/helper/KaptchaTextCreator.java
+++ src/main/java/com/fun/framework/shiro/helper/KaptchaTextCreator.java
@@ -10,7 +10,7 @@ import java.util.Random;
  *
  * @author DJun
  */
-public class CaptchaTextCreator extends DefaultTextCreator {
+public class KaptchaTextCreator extends DefaultTextCreator {
     private static final String[] CNUMBERS = "0,1,2,3,4,5,6,7,8,9,10".split(",");
 
     @Override
